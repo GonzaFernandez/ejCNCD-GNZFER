bb3afbe01ec9f92a3de2b4db1cf1e97d3b1f1b04
Gonzalo Fernandez <gonza_ferpac@hotmail.com>
2024-01-03 23:25:29 -0300

    initial commit for the exercise

---
+++ app/src/main/java/com/example/ejercicio_peliculas_cencosud/ExerciseFilmsMain.kt
@@ -0,0 +1,8 @@
+package com.example.ejercicio_peliculas_cencosud
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExerciseFilmsMain: Application() {
+}
