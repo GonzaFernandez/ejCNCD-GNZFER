832e2fe52e7476f4fa146ad56f3d5752cedf726a
Gonzalo Fernandez <gonza_ferpac@hotmail.com>
2024-01-04 16:23:07 -0300

    Add hilt module for retrofit and room

---
+++ app/src/main/java/com/example/ejercicio_peliculas_cencosud/common/HiltModule.kt
@@ -0,0 +1,61 @@
+package com.example.ejercicio_peliculas_cencosud.common
+
+import android.content.Context
+import androidx.room.Room
+import com.example.ejercicio_peliculas_cencosud.BuildConfig
+import com.example.ejercicio_peliculas_cencosud.common.Constants.MOVIES_DATABASE
+import com.example.ejercicio_peliculas_cencosud.dashboard.data.repository.api.client.MoviesApiClient
+import com.example.ejercicio_peliculas_cencosud.dashboard.data.repository.database.MoviesDatabase
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HiltModule {
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = GsonBuilder().setLenient().create()
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient().newBuilder()
+            .connectTimeout(1L, TimeUnit.MINUTES)
+            .readTimeout(1L, TimeUnit.MINUTES)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(gson: Gson, client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.FILMS_API_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideSubscriptionService(retrofit: Retrofit): MoviesApiClient =
+        retrofit.create(MoviesApiClient::class.java)
+
+    @Provides
+    @Singleton
+    fun provideRoom(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, MoviesDatabase::class.java, MOVIES_DATABASE).build()
+
+    @Provides
+    @Singleton
+    fun provideMovieDatabase(database: MoviesDatabase) = database.getMovieDao()
+
+}
